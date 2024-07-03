753fa3eca6ed7f85bccbb117558275f947e8df51
Ajala Tumininu <ajalatumininu@gmail.com>
2024-07-03 06:01:17 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,64 @@
-## Put comments here that give an overall description of what your
-## functions do
+# This R program defines functions to create a special matrix object that can cache its inverse and compute the inverse of the matrix, caching the result for future use. 
+# The program includes functions makeCacheMatrix to create the special matrix object, cacheSolve to compute and cache the inverse, and a test function to demonstrate the functionality.
 
-## Write a short comment describing this function
-
+# Function to create a special matrix object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  cache <- NULL
+  
+  set <- function(y) {
+    x <<- y
+    cache <<- NULL
+  }
+  
+  get <- function() {
+    x
+  }
+  
+  setInverse <- function(inverse) {
+    cache <<- inverse
+  }
+  
+  getInverse <- function() {
+    cache
+  }
+  
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
-
-## Write a short comment describing this function
-
+# Function to compute the inverse of the special matrix and cache the result
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  cache <- x$getInverse()
+  
+  if (!is.null(cache)) {
+    message("Retrieving cached data")
+    return(cache)
+  }
+  
+  data <- x$get()
+  inverse <- solve(data, ...)
+  x$setInverse(inverse)
+  inverse
 }
+
+# Test function to demonstrate the functionality
+test <- function() {
+  base_matrix <- makeCacheMatrix(matrix(1:4, 2, 2))
+  
+  base_matrix$get()
+  base_matrix$getInverse()
+  
+  cacheSolve(base_matrix)
+  
+  base_matrix$set(matrix(c(0, 5, 99, 66), nrow = 2, ncol = 2)) # Modify the existing matrix
+  
+  cacheSolve(base_matrix)   # Compute, cache, and return the new matrix inverse
+  base_matrix$get()         # Return the matrix
+  base_matrix$getInverse()  # Return the matrix inverse
+  
+  base_matrix$get() %*% base_matrix$getInverse() # Return the identity matrix
+}
+
+# Run the test function
+test()
